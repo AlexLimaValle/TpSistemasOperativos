fafc69d11d559233215c16b6aa2a2de661e1ff80
alexlimavalle <alexlimavalle@gmail.com>
2024-11-09 14:12:45 -0300

    agrego envio de mensajes entre dos usuarios

--- punto4/chatArchivo.c
+++ punto4/chatArchivo.c
@@ -10,15 +10,15 @@ typedef struct envio{
     char mensaje[256];
 }envio;
 
-void escribirEnChat(char,envio);
-void leerChat(char);
+void escribirEnChat(const char*,envio*);
 
 int main(int argc,char **argv){
-    envio usuario;
+    envio *usuario = malloc(sizeof(envio));
     char nombreUsuario[40];
+    const char *nombre = "chatArch.txt";
     printf("ingrese nombre de usuario: \n");
     scanf("%s",nombreUsuario);
-    strcpy(usuario.user,nombreUsuario);
+    strcpy(usuario->user,nombreUsuario);
     int eleccion;
     while(eleccion != 3){
         printf("1) enviar mensaje\n");
@@ -26,8 +26,10 @@ int main(int argc,char **argv){
         printf("3) salir\n");
         printf("Ingrese su eleccion: \n");
         scanf("%i",&eleccion);
+        getchar();
         switch (eleccion){
             case 1:
+                escribirEnChat(nombre,usuario);
                 break;
             case 2:
                 break;
@@ -35,6 +37,17 @@ int main(int argc,char **argv){
                 break;
         }
     }
-
     return 0;
+}
+
+void escribirEnChat(const char *nombre,envio *perfil){
+    FILE *archivo;
+    //abrir archivo creado
+    char mensajeAEscribir[256];
+    archivo = fopen(nombre,"a");
+    printf("[%s] -> ",perfil->user);
+    fgets(perfil->mensaje,sizeof(perfil->mensaje),stdin);
+    perfil->mensaje[strlen(perfil->mensaje)-1] = '\0';
+    fprintf(archivo,"[%s] => %s\n",perfil->user,perfil->mensaje);
+    fclose(archivo);
 }
