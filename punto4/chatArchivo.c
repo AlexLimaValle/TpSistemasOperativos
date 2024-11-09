6174772dff781d97d8cdc71ce58c9e6868df5e15
alexlimavalle <alexlimavalle@gmail.com>
2024-11-09 15:15:44 -0300

    punto 4 finalizado

--- punto4/chatArchivo.c
+++ punto4/chatArchivo.c
@@ -5,12 +5,14 @@
 #include <fcntl.h>
 #include <string.h>
 
+//estructura simple de envio, para asociar usuario con mensaje
 typedef struct envio{
     char user[40];
     char mensaje[256];
 }envio;
 
 void escribirEnChat(const char*,envio*);
+void leerHistorial(const char*,envio*);
 
 int main(int argc,char **argv){
     envio *usuario = malloc(sizeof(envio));
@@ -21,8 +23,9 @@ int main(int argc,char **argv){
     strcpy(usuario->user,nombreUsuario);
     int eleccion;
     while(eleccion != 3){
+        printf("****** CHAT ENTRE USUARIOS ******\n");
         printf("1) enviar mensaje\n");
-        printf("2) mostrar mensaje\n");
+        printf("2) mostrar historial mensajes\n");
         printf("3) salir\n");
         printf("Ingrese su eleccion: \n");
         scanf("%i",&eleccion);
@@ -32,22 +35,39 @@ int main(int argc,char **argv){
                 escribirEnChat(nombre,usuario);
                 break;
             case 2:
+                leerHistorial(nombre,usuario);
                 break;
             case 3:
                 break;
         }
     }
+    free(usuario);
     return 0;
 }
 
 void escribirEnChat(const char *nombre,envio *perfil){
     FILE *archivo;
-    //abrir archivo creado
-    char mensajeAEscribir[256];
-    archivo = fopen(nombre,"a");
-    printf("[%s] -> ",perfil->user);
-    fgets(perfil->mensaje,sizeof(perfil->mensaje),stdin);
-    perfil->mensaje[strlen(perfil->mensaje)-1] = '\0';
-    fprintf(archivo,"[%s] => %s\n",perfil->user,perfil->mensaje);
+    do{
+        //abrir archivo creado
+        archivo = fopen(nombre,"a");
+        printf("[%s] -> ",perfil->user);
+        fgets(perfil->mensaje,sizeof(perfil->mensaje),stdin);
+        perfil->mensaje[strlen(perfil->mensaje)-1] = '\0';
+        if(strncmp(perfil->mensaje,"salir",5) != 0){
+            fprintf(archivo,"[%s] => %s\n",perfil->user,perfil->mensaje);
+        }else{
+            fprintf(archivo,"[%s] => --- SALIO DE CONVERSACION ---\n",perfil->user);
+        }
+        fclose(archivo);
+    }while(strncmp(perfil->mensaje,"salir",5));
+}
+
+void leerHistorial(const char* nombre,envio *perfil){
+    FILE *archivo;
+    archivo = fopen(nombre,"r");
+    char parrafos[256];
+    while(fgets(parrafos,sizeof(parrafos),archivo)){
+        printf("%s\n",parrafos);
+    }
     fclose(archivo);
 }
